b2b723ea2ee399ba78051b418f64603f97a66aa8
bachtx12 <v.bachtx12@vinai.io>
2021-06-02 16:16:58 +0700

    fixed num nearest neighbors

--- train_part_segmentation.py
+++ train_part_segmentation.py
@@ -50,7 +50,7 @@ def parse_args():
                         help='dropout rate')
     parser.add_argument('--emb_dims', type=int, default=1024, metavar='N',
                         help='Dimension of embeddings')
-    parser.add_argument('--k', type=int, default=40, metavar='N',
+    parser.add_argument('--k', type=int, default=20, metavar='N',
                         help='Num of nearest neighbors to use')
     parser.add_argument('--model_path', type=str, default='', metavar='N',
                         help='Pretrained model path')
